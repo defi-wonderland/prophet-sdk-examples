3b85ca1f6de6c3916b14af4deb0635f1e11af046
0xRaccoon <raccoon@defi.sucks>
2023-11-29 11:02:41 +0100

    chore: update prophet libs
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

--- src/scripts/4-finalize-request.ts
+++ src/scripts/4-finalize-request.ts
@@ -1,5 +1,5 @@
 import { address } from '../constants';
-import { ProphetSDK } from '@defi-wonderland/prophet-sdk/dist';
+import { ProphetSDK } from '@defi-wonderland/prophet-sdk/dist/src';
 import { ContractRunner } from 'ethers-v6';
 import { getSigner } from '../helpers';
 
@@ -16,16 +16,18 @@ import { getSigner } from '../helpers';
   const signer = (await getSigner()) as unknown as ContractRunner;
 
   // Instantiate the sdk with the oracle address
-  const sdk = new ProphetSDK(signer, address.deployed.ORACLE);
+  const sdk = new ProphetSDK(signer, address.deployed.ORACLE, {});
 
   // Get the last request
   const requestCount = Number(await sdk.helpers.totalRequestCount());
-  const request = (await sdk.helpers.listRequests(requestCount - 1, 1))[0];
+  const requestId = (await sdk.helpers.listRequestIds(requestCount - 1, 1))[0];
+  const responseId = (await sdk.helpers.getResponseIds(requestId))[0];
 
-  const responseId = (await sdk.helpers.getResponseIds(request.requestId))[0];
+  const requestWithId = await sdk.helpers.getRequest(requestId);
+  const responseWithId = await sdk.helpers.getResponse(responseId);
 
   // Finalize the request using the sdk
-  const result = await sdk.helpers.finalize(request.requestId, responseId);
+  const result = await sdk.helpers.finalize(requestWithId.request, responseWithId.response);
 
   console.log(result);
 })();
